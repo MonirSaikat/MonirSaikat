2980a890b887f6eab1a6fed3e226f1eaf0265b9e
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-03 23:21:29 +0600

    added label on button

--- components/Navbar.js
+++ components/Navbar.js
@@ -35,7 +35,7 @@ export const Navbar = () => {
       <div className="container flex flex-wrap justify-between mx-auto items-center">
         <Logo />
 
-        <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-xl text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+        <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-xl text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-label='Toggler' aria-expanded="false">
           <RxHamburgerMenu />
         </button>
 
