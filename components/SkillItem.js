c992ac438607aee7438e288831a3fa7034a7f891
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-03 23:22:37 +0600

    added alt to img

--- components/SkillItem.js
+++ components/SkillItem.js
@@ -2,7 +2,7 @@ export const SkillItem = ({ skill }) => {
   return(
     <div className='py-5'>
       <div className={`mx-8`}>
-        <img src={skill.image.src} alt="" className='w-24' />
+        <img src={skill.image.src} alt="Monir Saikat" className='w-24' />
       </div>
     </div>
   );
