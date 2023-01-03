0d47bb77f2028125dd26064b15153698c062eddb
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-03 14:47:21 +0600

    form setup

---
+++ components/Input.js
@@ -0,0 +1,35 @@
+import classNames from 'classnames';
+
+export const Input = ({ label, className, register, errors, long, ...rest }) => {
+  const classes = classNames('border-2 rounded-md py-2 px-4 w-full outline-none', {
+    'border-red-500': errors[label]?.message
+  });
+
+  const renderTextarea = (
+    <textarea
+      className={classes}
+      id={label}
+      type='text'
+      {...register(label, { required: true })}
+      {...rest}
+    />
+  );
+
+  const renderInput = (
+    <input
+      className={classes}
+      id={label}
+      type='text'
+      {...register(label, { required: true })}
+      {...rest}
+    />
+  );
+
+  return (
+    <div className={`${className} mb-3`}>
+      <label className='block mb-2 text-xl capitalize' htmlFor={label}>{label}</label>
+      {long ? renderTextarea : renderInput}
+      <small className="text-red-600 font-semibold">{errors[label]?.message}</small>
+    </div>
+  );
+};
--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,51 @@
 import { Meta } from 'components/Meta';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { Input } from 'components/Input';
+
+const schema = yup.object({
+  name: yup.string().required(),
+  email: yup.string().email().required(),
+  message: yup.string().required()
+}).required();
 
 const contactPage = () => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const onSubmit = (data) => {
+  };
+
   return(
     <div>
       <Meta title='Contact' />
-      <h1>Contact page</h1>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            register={register}
+            errors={errors}
+            placeholder="Enter your full name"
+            label="name"
+          />
+          <Input
+            register={register}
+            errors={errors}
+            placeholder="Enter your email address"
+            label="email"
+          />
+          <Input
+            long
+            register={register}
+            errors={errors}
+            placeholder="Enter your message"
+            label="message"
+          />
+
+          <button className="p-3 my-3 bg-sky-300">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
