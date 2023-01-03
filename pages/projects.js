07dfbf9d3838104b1fe9dc01b4ee28a5a89776ad
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-03 12:08:55 +0600

    added first project

--- pages/projects.js
+++ pages/projects.js
@@ -1,10 +1,35 @@
+import classNames from 'classnames';
 import { SectionTitle } from 'components/SectionTitle';
+import Link from 'next/link';
 import { projectsArray } from 'utils/projects';
+import { BiLinkExternal } from 'react-icons/bi';
+
+const ButtonLink = ({ children, url, className, external = false }) => {
+  const classes = classNames('border px-4 py-1 text-sm rounded-full inline-flex items-center', className);
+
+  return(
+    <Link
+      className={classes}
+      href={ url }
+      target={external ? '_blank' : '_self'}
+    >
+      { children }
+      <BiLinkExternal className='ml-1' />
+    </Link>
+  );
+}
+
 
 const ProjectItem = ({ project }) => {
   return(
-    <div>
-      <h2>{ project.title }</h2>
+    <div className='border p-4 rounded-md shadow-sm bg-white'>
+      <p className='text-gray-700 mb-2'>{ project.year }</p>
+      <Link href={project.liveUrl} className='text-xl text-gray-800 font-semibold hover:underline'>{ project.title }</Link>
+      <p className="my-3 text-gray-600">Course management website</p>
+      <ButtonLink external url={project.liveUrl} className='mr-2'>
+        GitHub
+      </ButtonLink>
+      <ButtonLink external url={project.liveUrl}>Demo</ButtonLink>
     </div>
   );
 };
@@ -17,7 +42,9 @@ const ProjectPage = () => {
   return(
     <div>
       <SectionTitle>Projects</SectionTitle>
-      {renderProjects}
+      <div className="grid grid-cols-2 gap-2">
+        {renderProjects}
+      </div>
     </div>
   );
 };
--- utils/projects.js
+++ utils/projects.js
@@ -1,9 +1,13 @@
+import courseBuzzImg1 from 'assets/projects/cb-1.png';
+
 export const projectsArray = [
   {
+    year: 2022,
     title: 'Course Buzz',
-    liveUrl: '',
-    githubUrl: '',
-    technologies: [],
-    images: [],
+    liveUrl: 'https://course-buzz.web.app/',
+    githubUrl: 'https://github.com/MonirSaikat/course-buzz-frontend',
+    githubBackendUrl: 'https://github.com/MonirSaikat/course-buzz-backend',
+    technologies: ['node.js', 'react', 'mongodb', 'firebase', 'react-router-dom'],
+    images: [courseBuzzImg1 ],
   }
 ];
