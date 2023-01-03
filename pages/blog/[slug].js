1dca8285440556c6096d5a8ddd93f414dc2de121
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-04 03:23:58 +0600

    added tailwind typography for post details page

---
+++ pages/blog/[slug].js
@@ -0,0 +1,50 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import { marked } from 'marked';
+import path from 'path';
+
+const PostPage = ({
+  frontmatter,
+  slug,
+  content
+}) => {
+  return(
+    <div className='prose dark:prose-invert'>
+      <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+    </div>
+  );
+};
+
+export default PostPage;
+
+export function getStaticPaths() {
+  const files = fs.readdirSync(path.join('posts'))
+
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace('.md', ''),
+    },
+  }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export function getStaticProps({ params: { slug } }) {
+  const markdownWithMeta = fs.readFileSync(
+    path.join('posts', slug + '.md'),
+    'utf-8'
+  )
+
+  const { data: frontmatter, content } = matter(markdownWithMeta)
+
+  return {
+    props: {
+      frontmatter,
+      slug,
+      content,
+    },
+  }
+}
