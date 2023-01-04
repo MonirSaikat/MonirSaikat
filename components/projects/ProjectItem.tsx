de1c5f334833bf58a10417759dd77ba25fbaa2d8
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-05 00:35:28 +0600

    re-arrange button

--- components/projects/ProjectItem.tsx
+++ components/projects/ProjectItem.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import classNames from "classnames";
 import { Project } from "interfaces/Project";
 import { AiOutlineLink } from "react-icons/ai";
-import { ButtonLink } from 'components/buttons/ButtonLink';
+import { ButtonLink } from "components/buttons/ButtonLink";
 
 interface ProjectItemProps {
   project: Project;
@@ -18,19 +18,25 @@ export const ProjectItem = ({ project }: ProjectItemProps) => {
       <p className="text-gray-600 text-sm mb-2 dark:text-gray-400">
         {project.year}
       </p>
-      <Link
-        href={`/projects/${project.slug}`}
-        className="text-xl text-gray-800 font-semibold hover:underline inline-flex items-center dark:text-gray-200"
-      >
-        {project.title}
-        <AiOutlineLink />
-      </Link>
+
+      <div className="flex justify-between items-center">
+        <Link
+          href={`/projects/${project.slug}`}
+          className="text-xl text-gray-800 font-semibold hover:underline inline-flex items-center dark:text-gray-200"
+        >
+          {project.title}
+          <AiOutlineLink />
+        </Link>
+        <ButtonLink url={project.liveUrl} external>
+          <>
+            <span className="mr-1">view</span>
+          </>
+        </ButtonLink>
+      </div>
+
       <p className="my-3 text-sm text-gray-600 dark:text-gray-300">
         {project.subtitle}
       </p>
-      <ButtonLink external url={project.liveUrl} className="mr-2">
-        Demo
-      </ButtonLink>
       <ButtonLink external url={project.githubUrl} className="mr-2">
         Code
       </ButtonLink>
