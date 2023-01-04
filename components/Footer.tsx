6603d0f529b21bbc9a1e3da83abf14e89592c43b
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-05 00:11:45 +0600

    links in array

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,45 +1,55 @@
-import { AiFillLinkedin } from "react-icons/ai";
 import Link from "next/link";
+import { AiFillLinkedin } from "react-icons/ai";
 import { RxGithubLogo } from "react-icons/rx";
 import { MdEmail } from "react-icons/md";
 import classNames from "classnames";
+
+const links = [
+  {
+    ariaLabel: "Visit my linkedin",
+    href: "https://www.linkedin.com/in/moniruzzamansaikat/",
+    icon: <AiFillLinkedin />,
+  },
+  {
+    for: "Github",
+    href: "https://github.com/MonirSaikat",
+    icon: <RxGithubLogo />,
+  },
+  {
+    for: "Email",
+    href: "mailto:monirsaikat1@gmail.com",
+    icon: <MdEmail />,
+  },
+];
 
 export const Footer = () => {
   const linkClasses = classNames(
     "text-3xl text-gray-700 hover:text-gray-800 mr-5 dark:text-gray-300"
   );
 
+  const renderCopyrightText = (
+    <p className="text-center font-semibold text-gray-600 dark:text-gray-300">
+      &copy; Monir Saikat | {new Date().getFullYear()}
+    </p>
+  );
+
   return (
     <footer className="py-5">
       <div className="flex justify-center my-3">
-        <Link
-          aria-label="Visit my linkedin"
-          href="https://www.linkedin.com/in/moniruzzamansaikat/"
-          className={linkClasses}
-          target="_blank"
-        >
-          <AiFillLinkedin />
-        </Link>
-        <Link
-          aria-label="Visit my github"
-          href="https://github.com/MonirSaikat"
-          className={linkClasses}
-          target="_blank"
-        >
-          <RxGithubLogo />
-        </Link>
-        <a
-          aria-label="Send me an email"
-          href="mailto:monirsaikat1@gmail.com"
-          className={`${linkClasses} mr-0`}
-          target="_blank"
-        >
-          <MdEmail />
-        </a>
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            aria-label={link.ariaLabel}
+            href={link.href}
+            className={linkClasses}
+            target="_blank"
+          >
+            <AiFillLinkedin />
+          </Link>
+        ))}
       </div>
-      <p className="text-center font-semibold text-gray-600 dark:text-gray-300">
-        &copy; Monir Saikat | {new Date().getFullYear()}
-      </p>
+
+      {renderCopyrightText}
     </footer>
   );
 };
