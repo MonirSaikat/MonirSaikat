58323609f5e6e5b73c48999ccee7c214fb6d8bed
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-05 00:56:21 +0600

    updated heading

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
           Hey, I' am Saikat
         </h1>
         <p className="text-xl text-gray-600 mb-5 dark:text-gray-200">
-          Full-stack software engineer 👨‍💻
+          Full-stack software developer 👨‍💻
         </p>
         <p className="text-xl text-gray-600 dark:text-gray-200">
           I help people turn their ideas into websites that work. Professional
