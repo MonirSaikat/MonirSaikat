a56d18f8769e4ea42ad3688db8420b7417071201
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-02-21 02:28:49 +0600

    updated post item shadow

--- components/blog/PostItem.tsx
+++ components/blog/PostItem.tsx
@@ -9,8 +9,8 @@ export const PostItem = ({ post }: PostItemProps) => {
   const { slug, title, date } = post.frontmatter;
 
   return (
-    <div className="bg-white p-4 rounded-md dark:bg-slate-700 shadow">
-      <h2 className="text-2xl font-semibold dark:text-gray-200">{title}</h2>
+    <div className="bg-white p-4 rounded-md dark:bg-slate-700 shadow-lg">
+      <h2 className="text-2xl text-gray-600 font-semibold dark:text-gray-200">{title}</h2>
       <p className="text-sm my-2 dark:text-gray-300">{date}</p>
       <ButtonLink url={`/blog/${slug}`}>Read now</ButtonLink>
     </div>
