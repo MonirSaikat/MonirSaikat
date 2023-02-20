54fd5913d12ca53e1828ff6b3a484525a7acde0a
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-02-21 00:10:09 +0600

    added header subtitle

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -3,7 +3,7 @@ export const Header = () => {
     <header className="py-8 min-h-[300px] flex items-center">
       <div>
         <h1 className="text-3xl md:text-5xl text-gray-700 mb-5 font-bold dark:text-gray-200">
-          Hey, I' am Saikat
+          Hey, I' am Monir Saikat
         </h1>
         <p className="text-xl text-gray-600 mb-5 dark:text-gray-200">
           Full-stack software developer 👨‍💻
@@ -11,6 +11,9 @@ export const Header = () => {
         <p className="text-xl text-gray-600 dark:text-gray-200">
           I help people turn their ideas into websites that work. Professional
           and Cost-Effective. Always.
+        </p>
+        <p className="text-sm font-light mt-2 text-gray-500">
+          ✅ PHP & Javascript freelance developer
         </p>
       </div>
       {/* <div>
